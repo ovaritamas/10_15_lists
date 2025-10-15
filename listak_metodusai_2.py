51ad8530273cbc65ccb7757cc96da60ebd5335cc
ovaritamas <ovari.tamas@diak.szbi-pg.hu>
2025-10-15 09:21:45 +0200

    lista_metodusok_sorted_stb

---
+++ listak_metodusai_2.py
@@ -0,0 +1,11 @@
+#honapok = ['január', 'február','március', 'április', 'május', 'június']
+honapok = [1, 3, 5, 2]
+
+sorted_honapok = sorted(honapok, reverse=True)
+print(honapok)
+print(sorted_honapok)
+#print(honapok.sort())
+
+print(honapok.index('március'))
+
+print("április" in honapok)
